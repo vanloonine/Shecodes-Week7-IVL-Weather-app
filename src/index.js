8d531ed8bc286597fa0dfc884f1522d0a9031337
vanloonine <120510585+vanloonine@users.noreply.github.com>
2023-01-02 15:35:40 +0100

    added current date in date

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+// Show today current of a city
+
 function displayTemp(response) {
   console.log(response);
   let nowTemp = document.querySelector("#temperature-now");
@@ -14,9 +16,36 @@ function displayTemp(response) {
   nowHumidity.innerHTML = `Humidity: ${response.data.temperature.humidity}%`;
   let nowWind = document.querySelector("#wind-now");
   nowWind.innerHTML = `Wind: ${Math.round(response.data.wind.speed)} km/h`;
+  let nowDate = document.querySelector("#date-now");
+  nowDate.innerHTML = formatDate(response.data.time * 1000);
 }
 
 let apiKey = "b1ta5fo0b2c2fb14c08155a243d01b42";
-let apiURL = `https://api.shecodes.io/weather/v1/current?query=lisbon&key=${apiKey}`;
+let apiURL = `https://api.shecodes.io/weather/v1/current?query=sydney&key=${apiKey}`;
 
 axios.get(apiURL).then(displayTemp);
+
+//Show current time
+
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
