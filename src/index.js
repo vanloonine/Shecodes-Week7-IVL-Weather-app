ef17f4f920f4bbab27f876ce6c97b70bf14b34e5
vanloonine <120510585+vanloonine@users.noreply.github.com>
2023-01-02 21:23:28 +0100

    search form working

--- src/index.js
+++ src/index.js
@@ -1,35 +1,5 @@
 // Show today current of a city
 
-function displayTemp(response) {
-  console.log(response);
-  let nowTemp = document.querySelector("#temperature-now");
-  nowTemp.innerHTML = Math.round(response.data.temperature.current);
-  let nowCity = document.querySelector("#city-name-search");
-  nowCity.innerHTML = response.data.city;
-  let nowDescription = document.querySelector("#description-now");
-  nowDescription.innerHTML = response.data.condition.description;
-  let nowFeels = document.querySelector("#feels-now");
-  nowFeels.innerHTML = `Feels like: ${Math.round(
-    response.data.temperature.feels_like
-  )}°C`;
-  let nowHumidity = document.querySelector("#humidity-now");
-  nowHumidity.innerHTML = `Humidity: ${response.data.temperature.humidity}%`;
-  let nowWind = document.querySelector("#wind-now");
-  nowWind.innerHTML = `Wind: ${Math.round(response.data.wind.speed)} km/h`;
-  let nowDate = document.querySelector("#date-now");
-  nowDate.innerHTML = "Last updated: " + formatDate(response.data.time * 1000);
-  let nowIcon = document.querySelector("#icon-now");
-  nowIcon.setAttribute("src", response.data.condition.icon_url);
-}
-
-let apiKey = "b1ta5fo0b2c2fb14c08155a243d01b42";
-let city = "Lisbon";
-let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
-
-axios.get(apiURL).then(displayTemp);
-
-//Show current time
-
 function formatDate(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
@@ -53,14 +23,43 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-// search form functionality
-
-function search(event) {
-  event.preventDefault();
-  let citySearch = document.querySelector("#search-form");
-  console.log(citySearch);
+function displayTemp(response) {
+  console.log(response);
+  let nowTemp = document.querySelector("#temperature-now");
+  nowTemp.innerHTML = Math.round(response.data.temperature.current);
+  let nowCity = document.querySelector("#city-name-search");
+  nowCity.innerHTML = response.data.city;
+  let nowDescription = document.querySelector("#description-now");
+  nowDescription.innerHTML = response.data.condition.description;
+  let nowFeels = document.querySelector("#feels-now");
+  nowFeels.innerHTML = `Feels like: ${Math.round(
+    response.data.temperature.feels_like
+  )}°C`;
+  let nowHumidity = document.querySelector("#humidity-now");
+  nowHumidity.innerHTML = `Humidity: ${response.data.temperature.humidity}%`;
+  let nowWind = document.querySelector("#wind-now");
+  nowWind.innerHTML = `Wind: ${Math.round(response.data.wind.speed)} km/h`;
+  let nowDate = document.querySelector("#date-now");
+  nowDate.innerHTML = "Last updated: " + formatDate(response.data.time * 1000);
+  let nowIcon = document.querySelector("#icon-now");
+  nowIcon.setAttribute("src", response.data.condition.icon_url);
 }
 
-let form = document.querySelector("#search-form");
-form.addEventListener("submit", search);
-console.log (form)
+function search(city) {
+  let apiKey = "b1ta5fo0b2c2fb14c08155a243d01b42";
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiURL).then(displayTemp);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let citySearch = document.querySelector("#city-name-form");
+  console.log(citySearch.value);
+  search(citySearch.value);
+}
+
+let form = document.querySelector("#form");
+form.addEventListener("submit", handleSubmit);
+console.log(form);
+
+search("Antwerp");
